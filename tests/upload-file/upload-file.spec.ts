02d8eb3a8c7525260b6b68e393a5c3bfe028c77c
punkpeye <ahoy@punkpeye.com>
2022-08-25 21:05:54 -0500

    test: drag and drop tests

--- tests/upload-file/upload-file.spec.ts
+++ tests/upload-file/upload-file.spec.ts
@@ -1,14 +1,28 @@
 import { resolve } from "node:path";
-import { test } from "@/tests/test";
+import { expect, test } from "@/tests/test";
+import { createDataTransfer } from 'playwright-utilities';
 
 test("upload a file", async ({ page }) => {
-  console.log(resolve(__dirname, "bar.png"));
-
   await page.goto("http://127.0.0.1:8080/upload-file/");
-  await page.locator('input[name="foo"]').click();
+  await page.locator('input[type="file"]').click();
   await page
-    .locator('input[name="foo"]')
+    .locator('input[type="file"]')
     .setInputFiles(resolve(__dirname, "bar.png"));
 
-  await page.click("input[type=submit]");
+  await expect(page.locator('input[type="file"]')).toHaveValue(/bar/);
+});
+
+test('upload file using drop event', async ({page}) => {
+  await page.goto("http://127.0.0.1:8080/upload-file/");
+
+  const dataTransfer = await createDataTransfer({
+    page,
+    filePath: resolve(__dirname, "bar.png"),
+    fileName: "bar.png",
+    fileType: "image/png",
+  });
+
+  await page.dispatchEvent("#drop_zone", "drop", { dataTransfer });
+
+  await expect(page.locator("text=bar.png")).toBeVisible();
 });
